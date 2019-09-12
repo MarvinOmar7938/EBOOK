02747d8f119a639d26afc97b876adcc4dcf7cc20
marvin omar tola pacheco <mtolapacheco@gmail.com>
2019-09-12 15:06:19 -0400

    create class test

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
-var a = 10;
-if (true) {
-    var a = 15;
-    console.log("Dentro del if a tiene un valor de " + a);
-}
-else {
-    //
-}
-console.log("Fuera del if a tiene un valor de " + a);
+var Curso = /** @class */ (function () {
+    function Curso() {
+    }
+    Curso.prototype.getInscritos = function () {
+        return this.inscritos;
+    };
+    Curso.prototype.setInscritos = function (inscritos) {
+        this.inscritos = inscritos;
+    };
+    Curso.prototype.addInscrito = function () {
+        this.inscritos++;
+    };
+    Curso.prototype.remInscrito = function () {
+        this.inscritos--;
+    };
+    return Curso;
+}());
+var cursoAngular = new Curso();
+cursoAngular.setInscritos(9);
+cursoAngular.addInscrito();
+console.log(cursoAngular.getInscritos());
--- test.ts
+++ test.ts
@@ -1,8 +1,26 @@
-var a =10;
-if(true){
-    var a=15;
-    console.log("Dentro del if a tiene un valor de " +a);
-}else{
-    //
-}
-console.log("Fuera del if a tiene un valor de "+a);
+class Curso{
+    public titulo:string;
+    public description:string;
+    public horas:number;
+    public inscritos:number;
+
+    public getInscritos(){
+        return this.inscritos;
+    }
+
+    public setInscritos(inscritos:number){
+        this.inscritos=inscritos;
+    }
+    public addInscrito(){
+        this.inscritos++;
+    }
+    public remInscrito(){
+        this.inscritos--;
+    }
+}    
+    var cursoAngular= new Curso();
+
+    cursoAngular.setInscritos(9);
+    cursoAngular.addInscrito();
+
+    console.log(cursoAngular.getInscritos());
